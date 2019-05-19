a14277151d908c0ec8b306376b236b4dc30d71ca
cd0932507137 <cd0932507137@yahoo.com.tw>
2019-05-19 16:55:31 +0800

    require、module exports 模組設計

---
+++ app.js
@@ -0,0 +1,6 @@
+var content = require('./data.js');
+var a = 1;
+
+console.log(a);
+console.log(content);
+console.log(content.obj);
---
+++ data.js
@@ -0,0 +1,7 @@
+var data = 2;
+var obj = {
+  id: 1,
+  name: '帥哥一號'
+}
+
+module.exports = obj;
