86faba192f230ed4f2123b321cfce2f955b2f6bf
cd0932507137 <cd0932507137@yahoo.com.tw>
2019-05-21 21:06:01 +0800

    Node.js 核心模組 - createServer (中)

--- http.js
+++ http.js
@@ -1,5 +1,6 @@
 var http = require('http');
-http.createServer((require, response) => {
+http.createServer((request, response) => {
+  console.log(request.url);
   response.writeHead(200, { 'Content-Type': 'text/plain' });
   response.write('hi hi josh');
   response.end();
